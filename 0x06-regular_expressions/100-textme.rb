c09d7fce8a8d781bc41e4a9de93e2ef892479e04
joshua530 <joshuaomari5@gmail.com>
2022-07-26 18:58:27 +0300

    correct first pattern match

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+\d+)\] \[to:(\+\d+)\] \[flags:((:{0,1}\-{0,1}\d)+)\]/) { |g_1, g_2, g_2| puts "#{g_1},#{g_2},#{g_2}" }
+# puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+ARGV[0].scan(/\[from:(.+)\] \[to:(.+)\] \[flags:((:?\-?\d)+)\]/) { |g_1, g_2, g_3| puts "#{g_1},#{g_2},#{g_3}" }
