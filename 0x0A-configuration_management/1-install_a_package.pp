a327adea0821dc72b910c2c1095c9926826c3836
joshua530 <joshuaomari5@gmail.com>
2022-09-16 21:21:54 +0300

    add descriptive comments

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,4 @@
+# installs flask using pip3
 package { 'flask':
   name    => 'flask',
   command => '/usr/bin/pip3 install Flask==2.1.0',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# kills the program 'killmenow'
 exec { 'kill-killmenow':
   command => '/usr/bin/pkill killmenow'
 }
