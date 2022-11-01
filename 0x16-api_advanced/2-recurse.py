d1e0148c3c2a3ce130148aee5efb0e064846e57c
joshua530 <joshuaomari5@gmail.com>
2022-11-01 17:27:49 +0300

    remove comments

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,20 +7,17 @@ import requests
 def recurse(subreddit, hot_list=[], after=''):
     '''returns a list containing the titles of all hot articles
     for a given subreddit'''
-    # get items on current page and store in list
-    # if there are no items, return empty list
-    # else items = current + recurse(items for next page)
-    # return items
-    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
-        subreddit, after)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    if after != '':
+        url += '?after={}'.format(after)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'Http Agent 1.1'})
-    res = requests.get(url, headers=headers).json()
-    top_10 = res.get('data', {}).get('children', None)
+    res = requests.get(url, headers=headers, allow_redirects=False).json()
+    articles = res.get('data', {}).get('children', None)
     after = res.get('data').get('after')
-    if top_10 is None or after is None:
+    if after is None:
         return
-    for post in top_10:
+    for post in articles:
         titles = [title for title in post.get('data').get('title')]
     hot_list += titles
     recurse(subreddit, hot_list, after)
