98d63ab1da7616bad6f74e4d47386aad2b7af7c2
joshua530 <joshuaomari5@gmail.com>
2022-11-10 19:47:05 +0300

    add descriptive comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,6 @@
+# fixes file limit for server
+
 exec { 'changes file limit':
-  command => 'sed -i s/15/10000/ /etc/default/nginx && sudo service nginx restart',
+  command => 'sed -i s/15/10000/ /etc/default/nginx && service nginx restart',
   path    => ['/bin/', '/usr/bin', '/usr/sbin']
 }
